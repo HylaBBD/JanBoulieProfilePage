edbf3d0bda8dca502a7bb1f69bfc0a0165eb1a83
Hyla Fourie <hyla@bbd.co.za>
2023-01-31 15:55:09 +0200

    added bored api

--- script/api.js
+++ script/api.js
@@ -16,19 +16,35 @@
 //     factContent.innerHTML = factFormat;
 
 // }
-
 let clickedButton = false;
+
+async function accessAPI() {
+    const response = await fetch("https://www.boredapi.com/api/activity/");
+    const data = await response.json();
+    console.log(data.activity);
+    const boredomAct = data.activity;
+    let shownp = document.getElementById("shown-p");
+    let hiddenp = document.getElementById("boredom");
+        if(hiddenp.className==="boredom"){
+            hiddenp.className += " visible";
+            shownp.className += "boredom";
+        }            
+
+    document.getElementById("dog-fact").textContent = boredomAct;
+}
+
 function buttonClicked() {
-    if(clickedButton){
-        
+    if (clickedButton) {
+        accessAPI()
     }
-    else{
-        var navId = document.getElementById("video");
+    else {
+        let navId = document.getElementById("video");
         if (navId.className === "") {
-          navId.className += "show-video";
+            navId.className += "show-video";
         } else {
-          navId.className = "";
-          clickedButton=true;
-        } 
+            navId.className = "";
+            clickedButton = true;
+        }
     }
-  }
+}
+
