3a10561d500699e0982a9773af3607186b5648b2
Hyla Fourie <hyla@bbd.co.za>
2023-02-01 08:45:43 +0200

    fixed some css and changed js to be a bit tidier

--- script/api.js
+++ script/api.js
@@ -1,21 +1,3 @@
-// const factContent = document.getElementById("dog-fact");
-
-// async function getFact() {
-//     const response = await fetch("http://dog-api.kinduff.com/api/facts");
-//     let responseFact = await response.json();
-//     console.log(responseFact);
-
-//     if (response) {
-//         factOUt(responseFact);
-//     }
-// }
-// function factOUt(fact) {
-//     let factFormat =
-//         `<p>${fact.facts}<p>`;
-
-//     factContent.innerHTML = factFormat;
-
-// }
 let clickedButton = false;
 
 async function accessAPI() {
@@ -24,23 +6,20 @@ async function accessAPI() {
     console.log(data.activity);
     const boredomAct = data.activity;
     let shownp = document.getElementById("shown-p");
-    let hiddenp = document.getElementById("boredom");
-        if(hiddenp.className==="boredom"){
-            hiddenp.className += " visible";
-            shownp.className += "boredom";
-        }            
+    shownp.textContent="Sorry about that ;) Here's something to do if you are bored:"
 
-    document.getElementById("dog-fact").textContent = boredomAct;
+    document.getElementById("shown-h1").textContent = "Xd"
+    document.getElementById("activity").textContent = boredomAct;
 }
 
-function buttonClicked() {
+async function buttonClicked() {
     if (clickedButton) {
-        accessAPI()
+        await accessAPI()
     }
     else {
         let navId = document.getElementById("video");
         if (navId.className === "") {
-            navId.className += "show-video";
+            navId.classList.add("show-video");
         } else {
             navId.className = "";
             clickedButton = true;
