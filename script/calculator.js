852208873892f98fbe145bf829798f408ebe59dc
Hyla Fourie <hyla@bbd.co.za>
2023-02-02 11:20:07 +0200

    display working and own eval created

--- script/calculator.js
+++ script/calculator.js
@@ -1,14 +1,80 @@
 let calculation ="";
+let storedCalc="";
+let stackCalc=[];
+let storedOperation=[];
+let cumulativeArr=[];
+
+function populateCalc(rawVal){
+    console.log("butts");
+    floatVal=parseFloat(rawVal);
+    console.log(floatVal);
+    stackCalc.push(floatVal);
+    console.log(stackCalc)
+}
+function currentCalc(index,cumuNum){
+    let nextItem = stackCalc[index+1];
+    let operation = storedOperation[index];
+    console.log("-------------")
+    console.log(cumuNum)
+    console.log(operation)
+    console.log(nextItem)
+    
+    console.log("------------------")
+    let outcome=0;
+    switch(operation){
+        case "+":
+            console.log("+");
+            outcome=cumuNum+nextItem;
+            return outcome;
+        case "-":
+            console.log("-");
+            outcome=cumuNum-nextItem;
+            return outcome;
+        case "/":
+            console.log("/");
+            outcome=cumuNum/nextItem;
+            return outcome;
+        case "*":
+            console.log("*");
+            outcome=cumuNum*nextItem;
+            console.log(outcome);
+            return outcome;
+    }
+    return NaN;
+}
+
+function myEval(){
+    let outcome =stackCalc[0];
+    if (storedOperation){
+        for (let i =0;i<storedOperation.length;i++){
+            outcome= currentCalc(i,outcome);
+        }
+    }
+    return outcome;
+}
+
+function calculateResult(){
+    let resultMyEval= myEval().toString();
+    console.log("result",resultMyEval)
+    calculation=resultMyEval
+    // document.getElementById("calculator-content").textContent="jjjjjjjjjjjjjj"
+    // document.getElementById("calculator-content").textContent = resultMyEval;
+    calculation="";
+    return resultMyEval;
+}
 
 function readIn(id){
-    let value=document.getElementById(id).value
+    let value=document.getElementById(id).value;
     if(value==="clear" || value==="delete"){
         if(value==="clear"){
             calculation=""
+            storedCalc=""
+            stackCalc=[]
+            storedOperation=[]
             document.getElementById("calculator-content").textContent = "ENTER CALCULATION"
         }
         else{
-            calculation = calculation.slice(0,calculation.length-1);
+            calculation = calculation.slice(0,calculation.length-1);    
             document.getElementById("calculator-content").textContent = calculation;
             if(calculation=""){
                 document.getElementById("calculator-content").textContent = "ENTER CALCULATION"
@@ -16,14 +82,35 @@ function readIn(id){
         }
     }
     else{
+        floatValue = parseFloat(value);
+        // console.log(floatValue)
         calculation+=value;
+        
+        if(!isNaN(floatValue)){
+            console.log("inif")
+            storedCalc+=value;
+        }
+        else{
+            console.log("inelse")
+            if(value=="."){
+                console.log("ininin")
+                storedCalc+=value;
+            }
+            else{
+                populateCalc(storedCalc);
+                console.log("storednum",storedCalc)
+                storedCalc="";
+                if(value=="="){
+                    
+                    calculation=calculateResult()
+                    stackCalc=[]
+                    storedOperation=[]
+                }
+                else{
+                    storedOperation.push(value);
+                }
+            }
+        }
         document.getElementById("calculator-content").textContent = calculation;
     }
-
-}
-
-function calculateResult(){
-    result = eval(calculation)
-    document.getElementById("calculator-content").textContent = " = "  + result
-    calculation="";
 }
